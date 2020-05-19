bf4160c376a16d5cc5491de626c3e6317833be49
Camila Paixão <mila.paixao.cunha@gmail.com>
2020-05-19 01:11:02 -0300

    criando til

---
+++ til/color-range/js/script.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', start);
+
+
+function start() {
+  //pegando os inputs
+  let inputRed = document.getElementById('red');
+  let inputGreen = document.getElementById('green');
+  let inputBlue = document.getElementById('blue');
+
+  let body = document.querySelector('body');
+  body.style.backgroundColor = `rgb(${inputRed.value},${inputGreen.value},${inputBlue.value})`;
+
+  function insertColor() {
+    body.style.backgroundColor = `rgb(${inputRed.value},${inputGreen.value},${inputBlue.value})`;
+    function insertValues() {
+      document.getElementById('numberRed').value = inputRed.value;
+      document.getElementById('numberGreen').value = inputGreen.value;
+      document.getElementById('numberBlue').value = inputBlue.value;
+    }
+    insertValues();
+  }
+
+  inputRed.addEventListener('input', (event) => {
+    insertColor();
+  });
+  inputGreen.addEventListener('input', (event) => {
+    insertColor();
+  });
+  inputBlue.addEventListener('input', (event) => {
+    insertColor();
+
+  });
+}
